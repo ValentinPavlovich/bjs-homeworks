871228feab0c9fa012505d0b4bf036219c88bebe
ValentinPavlovich <valentin_72@mail.ru>
2019-04-22 10:58:39 +0300

    Task 1.3

--- variables/main.js
+++ variables/main.js
@@ -2,6 +2,7 @@
 let algebra = 5;
 let geography = 4;
 let physics = 4;
+let arithmeticMean;
 
 arithmeticMean = (algebra + geography + physics) / 3;
 
@@ -9,6 +10,18 @@ console.log(arithmeticMean);
 
 // Задача № 2
 let name = 'Валентин';
-let message = `Привет, мир! Меня зовут ${name}.`;
+let message;
+
+message = `Привет, мир! Меня зовут ${name}.`;
 
 console.log(message);
+
+// Задача № 3
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+
+result = x * y + 5 * z + x - 1;
+
+console.log(result);
