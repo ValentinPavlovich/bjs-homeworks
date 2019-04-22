802751d555f39cf9ca6be845a462908d722810ac
ValentinPavlovich <valentin_72@mail.ru>
2019-04-22 10:46:06 +0300

    Task 1.2

--- variables/main.js
+++ variables/main.js
@@ -1,3 +1,4 @@
+// Задача № 1
 let algebra = 5;
 let geography = 4;
 let physics = 4;
@@ -5,3 +6,9 @@ let physics = 4;
 arithmeticMean = (algebra + geography + physics) / 3;
 
 console.log(arithmeticMean);
+
+// Задача № 2
+let name = 'Валентин';
+let message = `Привет, мир! Меня зовут ${name}.`;
+
+console.log(message);
