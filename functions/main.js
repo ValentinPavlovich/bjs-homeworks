f56a952933a10043ff4e009a8177ac8c491e2b96
ValentinPavlovich <valentin_72@mail.ru>
2019-05-05 16:20:15 +0300

    Task 1.3-2

--- functions/main.js
+++ functions/main.js
@@ -42,3 +42,41 @@ function showSolutionsMessage(a,b,c) {
 showSolutionsMessage(1,2,3);
 showSolutionsMessage(7,20,-3);
 showSolutionsMessage(2,4,2);
+
+// Задача №2. Журнал успеваемости
+function getAverageScore(data) {
+    console.log('\n');
+
+    let averageSumOfMarks = 0;
+    let k = 0;
+    for (let prop in data) {
+        let value = data[ prop ];
+        
+        let sumOfMarks = 0;
+        for (let i = 0; i < value.length; i++) {
+            sumOfMarks += value[i]
+        }
+        
+        let averageMark = sumOfMarks /value.length;
+        console.log(`${prop}: ${averageMark}`);
+
+        averageSumOfMarks += averageMark;
+        k++;
+    }
+    
+    data.average = averageSumOfMarks/k;
+    console.log('average: ' + data.average);
+    return data;
+}
+
+console.log(getAverageScore({
+    algebra: [2, 4, 5, 2, 3, 4],
+    geometry: [2, 4, 5],
+    russian: [3, 3, 4, 5],
+    physics: [5, 5],
+    music: [2, 2, 6],
+    english: [4, 4, 3],
+    poetry: [5, 3, 4],
+    chemistry: [2],
+    french: [4, 4]
+}));
