de970c1972e7e2f57390eeed2ad9184b59109465
ValentinPavlovich <valentin_72@mail.ru>
2019-05-15 00:08:43 +0300

    Task 2.2, 1

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -7,7 +7,15 @@ function initCheckBirthday() {
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
+    // код для задачи №3
+    let now = Date.now();
+    let dateOfBirthday = new Date(birthday);
+    let birthday1 = dateOfBirthday.getTime();
+    let diff = now - birthday1;
+    let averageSum = ((3 * 31536000000) + 31622400000) / 4;
+    let age = Math.floor(diff / averageSum);
+    
+    return age >= 18;
 }
 
 function initPrintAnimalSound() {
@@ -21,7 +29,14 @@ function initPrintAnimalSound() {
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+    // код для задачи №1
+    let sound = animal.sound;
+
+    if (sound === undefined) {
+        return null;
+    } else {
+        return sound;
+    }
 }
 
 function initCalculateStatement() {
@@ -35,5 +50,15 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+    // код для задачи №2
+    let sumOfMarks = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sumOfMarks += parseInt(marks[i]);
+    }
+
+    let average = sumOfMarks / marks.length;
+    let roundedAverage = Math.round(average);
+
+    return roundedAverage;
 }
